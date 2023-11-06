1bc3d8251df53b78123684a9b750ecf612fb8312
Jarryd Baillie <jarryd@glucode.com>
2023-11-06 10:04:07 +0200

    Initial Commit

---
+++ VitalityOnboardingAssessment/VitalityOnboardingAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalityOnboardingAssessmentApp.swift
+//  VitalityOnboardingAssessment
+//
+//  Created by Jarryd Baillie on 2023/11/06.
+//
+
+import SwiftUI
+
+@main
+struct VitalityOnboardingAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
